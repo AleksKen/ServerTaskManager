d175f532f4dcda4b7791f2300e6b7cdac41b2217
AleksKen <aleks.ken2017@mail.ru>
2025-01-03 17:45:17 +0500

    Разворачивание окружения

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,8 @@ dependencies {
 	implementation("net.datafaker:datafaker:2.0.2")
 	implementation("org.instancio:instancio-junit:3.6.0")
 	// API documentation
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
+	implementation("org.springdoc:springdoc-openapi-starter-common:2.1.0")
 }
 
 sentry {
