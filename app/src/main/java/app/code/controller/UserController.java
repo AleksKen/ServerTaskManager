8d5d188644d15885ad5517fd13ca842cb66dfedd
AleksKen <aleks.ken2017@mail.ru>
2025-05-04 13:42:23 +0300

    CROSS для удаленного фронта

--- app/src/main/java/app/code/controller/UserController.java
+++ app/src/main/java/app/code/controller/UserController.java
@@ -23,7 +23,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/users")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class UserController {
     @Autowired
     private UserService userService;
